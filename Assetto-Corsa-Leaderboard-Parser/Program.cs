89a83229053b8a8af899e53d544583455be9533f
NinePiece2 <romit.sagu@hotmail.com>
2024-12-29 16:22:20 -0500

    Add project files.

---
+++ Assetto-Corsa-Leaderboard-Parser/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Assetto_Corsa_Leaderboard_Parser
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
